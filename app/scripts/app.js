643c548e3629f9f1071ff136ac6b00bab187bbb5
Veena Bharathi Akilla <veenabharathi93@gmail.com>
2018-11-13 16:21:09 +0530

    Ch4-8 promise with all quiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -61,12 +61,24 @@ Hint: you'll probably still need to use .map.
      */
     getJSON('../data/earth-like-results.json')
     .then(function(response) {
-
-      addSearchHeader(response.query);
-
-      response.results.map(function(url) {
-        getJSON(url).then(createPlanetThumb);
-      });
+            var arrayOfPromises = response.results.map(function(url){
+              console.log(url);
+              return getJSON(url);
+            });
+            console.log(arrayOfPromises);
+            return Promise.all(arrayOfPromises);
+      })
+    .then(function(arrayOfPlanetData){
+      arrayOfPlanetData.forEach(function(planet){
+        console.log(planet);
+        createPlanetThumb(planet);
+          });
+      })
+    // .then(function(response){
+    //   console.log("thumbnails created");
+    // })
+    .catch(function(error){
+      console.log(error);
     });
-  });
+    });
 })(document);
